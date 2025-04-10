4990821abd348781c90dc39a408ec5ba7944d6ed
Pham Trung Hoang <chfam@edu.hse.ru>
2025-04-10 03:49:55 +0300

    Adjust form layout and improve button styling in auth screen

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -12,20 +12,23 @@ class _AuthScreenState extends State<AuthScreen> {
   final AuthService _authService = AuthService();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  final TextEditingController _confirmPasswordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+  TextEditingController();
+
+  bool _passwordVisible = false;
+  bool _confirmPasswordVisible = false;
+
   bool isLogin = true;
 
-  void _authenticate() async {
-    String email = _emailController.text.trim();
-    String password = _passwordController.text.trim();
-    String confirmPassword = _confirmPasswordController.text.trim();
+  final _formKey = GlobalKey<FormState>();
 
-    if (!isLogin && password != confirmPassword) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Пароли не совпадают")),
-      );
+  void _authenticate() async {
+    if (!_formKey.currentState!.validate()) {
       return;
     }
+
+    String email = _emailController.text.trim();
+    String password = _passwordController.text.trim();
 
     User? user;
     if (isLogin) {
@@ -59,47 +62,213 @@ class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Вход в приложение")),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(labelText: "Email"),
-              keyboardType: TextInputType.emailAddress,
-            ),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(labelText: "Пароль"),
-              obscureText: true,
-            ),
-            if (!isLogin)
-              TextField(
-                controller: _confirmPasswordController,
-                decoration: InputDecoration(labelText: "Подтвердите пароль"),
-                obscureText: true,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(16.0, 140.0, 16.0, 16.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    isLogin ? "Добро пожаловать!" : "Регистрация",
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 90),
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                      labelText: "Email",
+                      prefixIcon: Icon(Icons.email, color: Colors.grey[700]),
+                      filled: true,
+                      fillColor: Color(0x2AD1BCE4),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(14),
+                        borderSide: BorderSide(color: Colors.grey.shade300),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(14),
+                        borderSide: BorderSide(color: Colors.grey.shade300),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(14),
+                        borderSide:
+                        BorderSide(color: Colors.blueAccent, width: 1.6),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value == null || value
+                          .trim()
+                          .isEmpty) {
+                        return "Введите email";
+                      }
+                      final emailRegex = RegExp(r"^[\w.-]+@[\w.-]+\.\w{2,4}$");
+                      if (!emailRegex.hasMatch(value.trim())) {
+                        return "Неверный формат email";
+                      }
+                      return null;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    controller: _passwordController,
+                    obscureText: !_passwordVisible,
+                    decoration: InputDecoration(
+                      labelText: "Пароль",
+                      prefixIcon: Icon(Icons.lock),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _passwordVisible
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                          color: Colors.grey,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _passwordVisible = !_passwordVisible;
+                          });
+                        },
+                      ),
+                      filled: true,
+                      fillColor: Color(0x2AD1BCE4),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(14),
+                        borderSide: BorderSide(color: Colors.grey.shade300),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(14),
+                        borderSide: BorderSide(color: Colors.grey.shade300),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(14),
+                        borderSide:
+                        BorderSide(color: Colors.blueAccent, width: 1.6),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value == null || value
+                          .trim()
+                          .isEmpty) {
+                        return "Введите пароль";
+                      }
+                      return null;
+                    },
+                  ),
+                  ...(!isLogin
+                      ? [
+                    SizedBox(height: 16),
+                    TextFormField(
+                      controller: _confirmPasswordController,
+                      obscureText: !_confirmPasswordVisible,
+                      decoration: InputDecoration(
+                        labelText: "Подтвердите пароль",
+                        prefixIcon: Icon(Icons.lock_outline),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _confirmPasswordVisible
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                            color: Colors.grey,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _confirmPasswordVisible =
+                              !_confirmPasswordVisible;
+                            });
+                          },
+                        ),
+                        filled: true,
+                        fillColor: Color(0x2AD1BCE4),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(14),
+                          borderSide:
+                          BorderSide(color: Colors.grey.shade300),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(14),
+                          borderSide:
+                          BorderSide(color: Colors.grey.shade300),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(14),
+                          borderSide: BorderSide(
+                              color: Colors.blueAccent, width: 1.6),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (!isLogin &&
+                            (value == null || value
+                                .trim()
+                                .isEmpty)) {
+                          return "Повторите пароль";
+                        }
+                        if (!isLogin &&
+                            _passwordController.text.trim() !=
+                                value?.trim()) {
+                          return "Пароли не совпадают";
+                        }
+                        return null;
+                      },
+                    ),
+                  ]
+                      : []),
+                  SizedBox(height: 30),
+                  SizedBox(
+                    width: 250,
+                    child: ElevatedButton(
+                      onPressed: _authenticate,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blueAccent,
+                        padding: EdgeInsets.symmetric(vertical: 13),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        elevation: 3,
+                      ),
+                      child: Text(
+                        isLogin ? "Войти" : "Зарегистрироваться",
+                        style: TextStyle(fontSize: 15, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        isLogin = !isLogin;
+                      });
+                    },
+
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(0, 15.0, 0, 0),
+                      child: Text(isLogin
+                        ? "Нет аккаунта? Зарегистрироваться"
+                        : "Уже есть аккаунт? Войти"),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Divider(),
+                  SizedBox(height: 10),
+                  ElevatedButton.icon(
+                    onPressed: _signInWithGoogle,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(14)),
+                      elevation: 2,
+                      side: BorderSide(color: Colors.grey.shade300),
+                    ),
+                    icon: Image.asset('assets/images/google_logo.webp',
+                        height: 24),
+                    label: Text('Войти через Google',
+                        style: TextStyle(color: Colors.black87)),
+                  )
+                ],
               ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _authenticate,
-              child: Text(isLogin ? "Войти" : "Зарегистрироваться"),
             ),
-            TextButton(
-              onPressed: () {
-                setState(() {
-                  isLogin = !isLogin;
-                });
-              },
-              child: Text(isLogin ? "Нет аккаунта? Зарегистрироваться" : "Уже есть аккаунт? Войти"),
-            ),
-            Divider(),
-            ElevatedButton(
-              onPressed: _signInWithGoogle,
-              child: Text("Войти через Google"),
-            ),
-          ],
+          ),
         ),
       ),
     );
